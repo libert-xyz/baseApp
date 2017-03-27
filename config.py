25bfb082d4f5b201b55bccda4d56b0c9b341d991
Libert R Schmidt <rschmidt@nuvops.com>
2017-03-26 20:53:43 -0400

    prod config

--- config.py
+++ config.py
@@ -10,13 +10,14 @@ class Config(object):
 
 class ProductionConfig(Config):
 
-    DATABASE_URI = 'mysql://user@localhost/foo'
+    #DATABASE_URI = 'mysql://user@localhost/foo'
     #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     #SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s:3306/%s' %(DB_USERNAME,DB_PASSWORD,MARIA_D,BLOG_DATABASE_NAME)
-    DATABASE_NAME = 'MessagesApp'
+    #DATABASE_NAME = 'MessagesApp'
     #MARIA_D = '172.17.0.1'
-    DB_USERNAME = 'pkyeiuuliulvll'
-    DB_PASSWORD = '3V9brJhnXLABJPmP1nLEVp0kXL'
+    #DB_USERNAME = 'pkyeiuuliulvll'
+    #DB_PASSWORD = '3V9brJhnXLABJPmP1nLEVp0kXL'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'messagesProd.db')
 
 class DevelopmentConfig(Config):
     DEBUG = True
